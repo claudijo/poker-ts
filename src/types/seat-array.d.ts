26791a8d5f3295e1826f393317c0eaf893e92492
claudijo <claudijo.borovic@gmail.com>
2021-05-24 21:25:37 +0300

    Remove SeatArray file and TODO

--- src/types/seat-array.d.ts
+++ src/types/seat-array.d.ts
@@ -1,19 +1,3 @@
 import Player from "../lib/player";
 
 export type SeatArray = Array<Player | null>
-
-// TODO: Make class of this, including following functionality
-/*
-
-// Get first index of not null player
-auto seat = seat_index{_hand_players.begin().index()};
-
-// Iterate non null players with optional start index
-auto it = seat_array::iterator{_hand_players, _button};
-        ++it;
-        if (it.index() == num_seats) {
-            _button = _hand_players.begin().index();
-        } else {
-            _button = it.index();
-        }
- */
